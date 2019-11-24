2e47e06736693224f320155054f57453f9a04b71
aing03Wm <aingWM@hotmail.com>
2019-11-24 14:38:45 +0800

    新增调用 test() 函数以演示相关函数的执行

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 
 #include <stdio.h>
 
+#include "test.h"
 
 int main(int argc, char *argv[])
 {
     printf_s("Hello, World!\n");    // 遵循 C99 及之后的协议，采用带"_s"的字符串操作函数
-    
+
+    test();
+
     return 0;
 }
